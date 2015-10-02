b6a2f859e975d6a580a9c9f2e81861b12705d91a
jalesholanda <jales_holanda@me.com>
2015-10-02 20:15:26 -0300

    Mudança na tela de vegas no admin

--- invagasapp/admin.py
+++ invagasapp/admin.py
@@ -1,5 +1,14 @@
 from django.contrib import admin
-
 from invagasapp.models import Vagas
 
-admin.site.register(Vagas)
+
+class VagasAdmin(admin.ModelAdmin):
+
+
+    list_display = ('titulo', 'descricao', 'data_postagem')
+
+    list_filter = ['data_postagem']
+
+    search_fields = ('data_postagem', 'titulo')
+
+admin.site.register(Vagas, VagasAdmin)
